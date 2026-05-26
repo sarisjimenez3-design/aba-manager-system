eeddea6bedff3c8cbeb6f18f397b486aa30c99cd
unknown <saraJimenez@gmail.com>
2026-05-25 22:28:21 -0500

    chore: preparar servidor para railiway

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,8 @@ dotenv.config();
 
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
 });
